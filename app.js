ae4ee72f08de7346cdcb2c750eefd71a43dc14f3
Eddie Gathua <eddygathua@gmail.com>
2022-05-26 16:07:01 +0300

    Adding functionality to the buttons

---
+++ app.js
@@ -0,0 +1,8 @@
+const date = document.getElementById("date");
+const toggleButton = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+date.innerHTML = new Date().getFullYear();
+
+toggleButton.addEventListener("click", () => {});
